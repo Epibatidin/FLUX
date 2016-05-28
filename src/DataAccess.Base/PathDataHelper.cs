918ff0002c91fa7a70015305f4b56242060e4b10
Epibatidin <ferenc.mohos@gmail.com>
2016-05-28 20:36:59 +0200

    add some tests and fix the issue found while doing tests

--- src/DataAccess.Base/PathDataHelper.cs
+++ src/DataAccess.Base/PathDataHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace DataAccess.Base
+﻿using System;
+
+namespace DataAccess.Base
 {
     public class PathDataHelper
     {
@@ -8,11 +10,14 @@
 
             var extPos = absoluteFilePath.LastIndexOf('.');
             if (extPos == -1)
-                extPos = absoluteFilePath.Length;
-
-            pathData.Extension = absoluteFilePath.Substring(extPos);
-
-            var pureVirtualFilePath = absoluteFilePath.Substring(rootDirPath.Length, absoluteFilePath.Length - extPos);
+            {
+                extPos = absoluteFilePath.Length + 1;
+                pathData.Extension = String.Empty;
+            }
+            else
+                pathData.Extension = absoluteFilePath.Substring(++extPos);
+            
+            var pureVirtualFilePath = absoluteFilePath.Substring(rootDirPath.Length +1 ,extPos- rootDirPath.Length -2);
             pathData.PathParts = pureVirtualFilePath.Split('\\');
 
             return pathData;
---
+++ test/DataAccess.Tests/Base/PathDataHelperTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Base;
+using Extension.Test;
+using Xunit;
+using Assert = NUnit.Framework.Assert;
+using Is = NUnit.Framework.Is;
+
+namespace DataAccess.Tests.Base
+{
+    public class PathDataHelperTests : FixtureBase<PathDataHelper>
+    {
+        [Fact]
+        public void should_find_extension()
+        {
+            string path =
+                "D:\\Musik\\Musik - Die geordnet werden muss\\Amduscia\\2011 Amduscia-Death_Thou_Shalt_Die\\01_amduscia-damn_punks.mp3";
+
+            var result = SUT.FullPathToVirtualPathData(path, "");
+
+            Assert.That(result.Extension, Is.EqualTo("mp3"));
+        }
+
+        [Fact]
+        public void should_support_non_extension()
+        {
+            string path =
+                "D:\\Musik\\Musik - Die geordnet werden muss\\Amduscia\\2011 Amduscia-Death_Thou_Shalt_Die\\01_amduscia-damn_punks";
+
+            var result = SUT.FullPathToVirtualPathData(path, "");
+
+            Assert.That(result.Extension, Is.EqualTo(""));
+        }
+        
+        [Fact]
+        public void should_split_by_slashes()
+        {
+            string path =
+                "D:\\Musik\\Musik - Die geordnet werden muss\\Amduscia\\2011 Amduscia-Death_Thou_Shalt_Die\\01_amduscia-damn_punks.mp3";
+
+            var result = SUT.FullPathToVirtualPathData(path, "");
+
+            Assert.That(result.PathParts.Contains("2011 Amduscia-Death_Thou_Shalt_Die"), Is.True);
+        }
+
+        [Fact]
+        public void should_remove_root_part()
+        {
+            string path =
+                "D:\\Musik\\01_amduscia-damn_punks.mp3";
+
+            var result = SUT.FullPathToVirtualPathData(path, "D:\\Musik");
+
+            Assert.That(result.PathParts.Length, Is.EqualTo(1));
+        }
+
+        [Fact]
+        public void should_remove_root_part_and_set_path_part()
+        {
+            string path =
+                "D:\\Musik\\01_amduscia-damn_punks.mp3";
+
+            var result = SUT.FullPathToVirtualPathData(path, "D:\\Musik");
+
+            Assert.That(result.PathParts[0], Is.EqualTo("01_amduscia-damn_punks"));
+        }
+
+        [Fact]
+        public void should_remove_root_part_and_set_path_part_without_extension()
+        {
+            string path =
+                "D:\\Musik\\01_amduscia-damn_punks";
+
+            var result = SUT.FullPathToVirtualPathData(path, "D:\\Musik");
+
+            Assert.That(result.PathParts[0], Is.EqualTo("01_amduscia-damn_punks"));
+        }
+
+    }
+}
