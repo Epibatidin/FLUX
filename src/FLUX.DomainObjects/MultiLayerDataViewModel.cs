eff5fbf83e11c1b4cf9c442213b9c9f1c18da1ae
Epibatidin <ferenc.mohos@gmail.com>
2016-11-25 23:23:51 +0100

    finally so kind of stable UI

---
+++ src/FLUX.DomainObjects/MultiLayerDataViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace FLUX.DomainObjects
+{
+    public class MultiLayerDataViewModel
+    {
+        private int forLevel;
+        
+        public IList<string> Keys { get; set; }
+        private MultiLayerDataContainer Container { get; set; }
+        
+        public string AdditionalClass { get; set; }
+
+        public MultiLayerDataViewModel(MultiLayerDataContainer container,string additionalClass, int forLvl, string[] keys)
+        {
+            forLevel = forLvl;
+            Container = container;
+            Keys = keys;
+            AdditionalClass = additionalClass;
+        }
+
+        public string OriginalValue
+        {
+            get
+            {
+                return Container.GetOriginalValue(forLevel);
+            }
+        }
+
+        public IList<string> RetrieveData(string key)
+        {
+            List<string> list = null;
+
+            if (!Container.Data.TryGetValue(key, out list))
+                list = new List<string>();
+
+            return list;
+        }
+
+    }
+}
---
+++ src/FLUX.Web.MVC/TagHelpers/MultiLayerDataContainerTagHelper.cs
@@ -0,0 +1,88 @@
+﻿using DataStructure.Tree;
+using FLUX.DomainObjects;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLUX.Web.MVC.TagHelpers
+{
+    public class MultiLayerDataTagHelper : TagHelper
+    {
+        ICompositeViewEngine _viewEngine;
+
+        public MultiLayerDataTagHelper(ICompositeViewEngine viewEngine )
+        {
+            _viewEngine = viewEngine;
+        }
+
+        [ViewContext] public ViewContext ViewContext { get; set; }
+        
+        [HtmlAttributeName("model")]
+        public ITreeItem<MultiLayerDataContainer> Container { get; set; }
+
+        [HtmlAttributeName("keys")]
+        public string KeysAsString { get; set; }
+        
+        [HtmlAttributeName("class")]
+        public string Class { get; set; }
+        
+        [HtmlAttributeName("level")]
+        public int ForLevel { get; set; }
+
+
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+
+            string[] keyArray = null;
+            if (KeysAsString == null)
+                keyArray = new[] { Class };
+            else
+                keyArray = KeysAsString.Split(',');
+
+            var model = new MultiLayerDataViewModel(Container.Value, Class ,ForLevel, keyArray);
+            var viewContext = new ViewContext(ViewContext, ViewContext.View,
+                new ViewDataDictionary<MultiLayerDataViewModel>(ViewContext.ViewData, model), ViewContext.Writer);
+         
+            var view = _viewEngine.FindView(viewContext, "MultiLayerDataViewModel", false);
+            
+            await view.View.RenderAsync(viewContext);
+        }
+    }
+}
+
+
+
+
+//public async override void Process(TagHelperContext context, TagHelperOutput output)
+//{
+//    var sw = new StringWriter();
+
+//    // Create a new viewData (viewbag). This will be used in a new ViewContext to define the model we want
+//    ViewDataDictionary viewData = new ViewDataDictionary(ViewContext.ViewData, For.Model);
+
+//    // Generate a viewContext with our viewData
+//    var viewContext = new ViewContext(ViewContext, ViewContext.View, viewData, ViewContext.TempData, sw, new HtmlHelperOptions());
+
+//    // Use the viewContext to run the given ViewName
+//    output.Content.Append(new HtmlString(await viewContext.RenderPartialView(ViewName)));
+
+//}
+
+
+//public async static Task<string> RenderPartialView(this ViewContext context, string viewName, ICompositeViewEngine viewEngine = null, ViewEngineResult viewResult = null)
+//{
+//    viewEngine = viewEngine ?? context.HttpContext.RequestServices.GetRequiredService<ICompositeViewEngine>();
+
+//    viewResult = viewResult ?? viewEngine.FindPartialView(context, viewName);
+
+//    await viewResult.View.RenderAsync(context);
+
+//    return context.Writer.ToString();
+
+//}
