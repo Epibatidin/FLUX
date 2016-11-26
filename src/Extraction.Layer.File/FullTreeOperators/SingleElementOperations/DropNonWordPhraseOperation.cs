625c5a8033f780ee161e70e20caa6161fcfb9c9e
Epibatidin <ferenc.mohos@gmail.com>
2016-11-26 16:31:19 +0100

    make checking for voxels case insensitive

--- src/Extraction.Layer.File/FullTreeOperators/SingleElementOperations/DropNonWordPhraseOperation.cs
+++ src/Extraction.Layer.File/FullTreeOperators/SingleElementOperations/DropNonWordPhraseOperation.cs
@@ -1,19 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Extraction.DomainObjects.StringManipulation;
 using Extraction.Interfaces;
 
 namespace Extraction.Layer.File.FullTreeOperators.SingleElementOperations
-{
+{    
     public class DropNonWordPhraseOperation : IPartedStringOperation
     {
-        private static readonly HashSet<char> LettersMakingAValidWord = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü', 'y', '.' };
+        private class CharEqualityComparer : IEqualityComparer<char>
+        {
+            public bool Equals(char x, char y)
+            {
+                return Char.ToUpper(x) == Char.ToUpper(y);
+            }
+
+            public int GetHashCode(char obj)
+            {
+                return Char.ToUpper(obj).GetHashCode();
+            }
+        }
+
+
+        private static readonly HashSet<char> LettersMakingAValidWord = 
+            new HashSet<char>(new CharEqualityComparer()) { 'a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü', 'y', '.' };
 
         public void Operate(PartedString source)
         {
             for (int i = 0; i < source.Count; i++)
             {
                 string part = source[i];
-                if (1 >= part.Length && part.Length >= 5) continue;
+                if (part.Length > 4) continue;
                 // kontrolliere ob konsonanten enthalten sind 
 
                 // but what about dots ? like in acronyms ? 
