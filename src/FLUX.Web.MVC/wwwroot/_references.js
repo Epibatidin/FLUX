22502ff43572780ce2206164e1f5398484036309
Epibatidin <ferenc.mohos@gmail.com>
2017-02-25 00:32:47 +0100

    support for draagging titles between cds

--- src/FLUX.Web.MVC/wwwroot/_references.js
+++ src/FLUX.Web.MVC/wwwroot/_references.js
@@ -2,6 +2,7 @@
 /// <reference path="../gulpfile.js" />
 /// <reference path="js/configuration.js" />
 /// <reference path="js/datadelivery.js" />
+/// <reference path="js/dragdrop.js" />
 /// <reference path="js/notifications.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
---
+++ src/FLUX.Web.MVC/wwwroot/js/DragDrop.js
@@ -0,0 +1,82 @@
+﻿var pattern = /[0-9]+/g;
+function readId(trElement) {
+    var nameOfIdField = $('.idHolder', trElement).attr('name');
+
+    var ids = nameOfIdField.match(pattern);
+
+    for (var i = 0; i < ids.length; i++) {
+        ids[i] = parseInt(ids[i]);
+    }
+
+    return ids;
+}
+
+function rewriteNames(trElement, ids) {
+
+    // select all input elements 
+
+    $('input', trElement).each(function () {
+
+        var runner = 0;
+        this.name = this.name.replace(pattern, function () {
+            var id = ids[runner];
+            runner++;
+            return id;
+        });
+    });
+}
+
+
+function sortEnd(event, ui) {
+    var r = this;
+    var ids;
+
+    // check if first element - we take the element after as id provider 
+    var prev = ui.item.prev();
+    if (prev.length == 0)
+    {
+        var next = ui.item.next();
+        ids = readId(next);
+        ids[ids.length - 1] = 0;
+    }
+    else // check if middle or last element - we take the element before as id provider 
+    {
+        ids = readId(prev);
+        ids[ids.length - 1] = ids[ids.length - 1] + 1;
+    }
+
+    // ich hab jetzt die id des ersten elements 
+    // ich renne jetzt so lange nach hinten bis es kein hinten mehr gibt 
+    var runner = ui.item;
+    while (true) {
+        rewriteNames(runner, ids);
+        ids[ids.length - 1] = ids[ids.length - 1] + 1;
+        runner = runner.next();
+        if (runner.length == 0) break;
+    }
+}
+
+function sortStart(event, ui) {
+    var nameOfIdField = $('.idHolder', ui.item).attr('name');
+    
+    var ids = nameOfIdField.match(pattern);
+
+    ui.item.data('ids', ids);
+}
+
+
+$(document).ready(function () {
+    var wrapper = $(".CDWrapper");
+
+    wrapper.sortable({
+        revert: false,
+        items: "tr",
+        axis: 'y',
+        placeholder: "dragPlaceHolder",
+        tolerance: "pointer",
+        cursor: "move",
+        stop: sortEnd,
+        start: sortStart,
+    });
+    //wrapper.disableSelection();
+});
